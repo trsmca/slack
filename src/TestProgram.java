a4e89f37480c6adc8798c62f6df239e38cff7068
trsmca <trsmca35@gmail.com>
2019-02-06 02:13:41 +0530

    Update TestProgram.java

--- src/TestProgram.java
+++ src/TestProgram.java
@@ -1,6 +1,12 @@
 public class MyClass {
   public static void main(String[] args) {
-    int i=10;
+    int foo(int a) {
+  int b = 12;
+  if (a == 1) {
+    return b;
+  }
+  return b;  // Noncompliant
+}
     System.out.println("Hello World");
   }
 } 
