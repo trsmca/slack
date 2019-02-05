c4bbf4bc6c19ac711133762723b956ebfdf95eda
trsmca <trsmca35@gmail.com>
2019-02-06 02:15:37 +0530

    Update TestProgram.java

--- src/TestProgram.java
+++ src/TestProgram.java
@@ -1,12 +1,10 @@
 public class MyClass {
   public static void main(String[] args) {
-    int foo(int a) {
-  int b = 12;
-  if (a == 1) {
-    return b;
-  }
-  return b;  // Noncompliant
-}
+   int target = -5;
+int num = 3;
+
+target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+target =+ num; // Noncompliant; target = 3
     System.out.println("Hello World");
   }
 } 
