e71a2cba9e3c72d97d141f03391bba486690eeb7
trsmca <trsmca35@gmail.com>
2019-02-20 00:13:43 +0530

    Update TestProgram.java

--- src/TestProgram.java
+++ src/TestProgram.java
@@ -1,7 +1,7 @@
 public class MyClass {
   public static void main(String[] args) {
    int target = -5;
-int num = 3;
+int num = 3
 
 target =- num;  // Noncompliant; target = -3. Is that really what's meant?
 target =+ num; // Noncompliant; target = 3
